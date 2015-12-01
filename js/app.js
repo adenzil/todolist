d462483f5b5b3813eb70a3ba01db8c5d2e97ffb1
Alister <alistercabral@gmail.com>
2015-12-01 13:29:34 +0530

    minor changes

--- js/app.js
+++ js/app.js
@@ -11,7 +11,7 @@ angular.module('todomvc', ['ngRoute'])
 
 		var routeConfig = {
 			controller: 'TodoCtrl',
-			templateUrl: 'todomvc-index.html',
+			templateUrl: 'index.html',
 			resolve: {
 				store: function (todoStorage) {
 					// Get the correct module (API or localStorage).
